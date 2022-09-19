291b1debf512aeb05bcc999062080751258194dc
ajsid2005 <113822270+ajsid2005@users.noreply.github.com>
2022-09-19 12:45:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,69 @@
-// Get the source image to be edited
-let image = document.getElementById('sourceImage');
+const fileInput = document.querySelector("#imageFileInput");
+const canvas = document.querySelector("#canvas");
+const canvasCtx = canvas.getContext("2d");
+const brightnessInput = document.querySelector("#brightness");
+const saturationInput = document.querySelector("#saturation");
+const blurInput = document.querySelector("#blur");
+const inversionInput = document.querySelector("#inversion");
 
-// Get the canvas for the edited image
-let canvas = document.getElementById('canvas');
+const settings = {};
+let image = null;
 
-// Get the 2D context of the image
-let context = canvas.getContext('2d');
+function resetSettings() {
+  settings.brightness = "100";
+  settings.saturation = "100";
+  settings.blur = "0";
+  settings.inversion = "0";
 
-// Get all the sliders of the image
-let brightnessSlider = document.getElementById("brightnessSlider");
-let contrastSlider = document.getElementById("contrastSlider");
-let grayscaleSlider = document.getElementById("grayscaleSlider");
-let hueRotateSlider = document.getElementById("hueRotateSlider");
-let saturateSlider = document.getElementById("saturationSlider");
-let sepiaSlider = document.getElementById("sepiaSlider");
+  brightnessInput.value = settings.brightness;
+  saturationInput.value = settings.saturation;
+  blurInput.value = settings.blur;
+  inversionInput.value = settings.inversion;
+}
+
+function updateSetting(key, value) {
+  if (!image) return;
+
+  settings[key] = value;
+  renderImage();
+}
+
+function generateFilter() {
+  const { brightness, saturation, blur, inversion } = settings;
+
+  return `brightness(${brightness}%) saturate(${saturation}%) blur(${blur}px) invert(${inversion}%)`;
+}
+
+function renderImage() {
+  canvas.width = image.width;
+  canvas.height = image.height;
+
+  canvasCtx.filter = generateFilter();
+  canvasCtx.drawImage(image, 0, 0);
+}
+
+brightnessInput.addEventListener("change", () =>
+  updateSetting("brightness", brightnessInput.value)
+);
+saturationInput.addEventListener("change", () =>
+  updateSetting("saturation", saturationInput.value)
+);
+blurInput.addEventListener("change", () =>
+  updateSetting("blur", blurInput.value)
+);
+inversionInput.addEventListener("change", () =>
+  updateSetting("inversion", inversionInput.value)
+);
+
+fileInput.addEventListener("change", () => {
+  image = new Image();
+
+  image.addEventListener("load", () => {
+    resetSettings();
+    renderImage();
+  });
+
+  image.src = URL.createObjectURL(fileInput.files[0]);
+});
+
+resetSettings();
